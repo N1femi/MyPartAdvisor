ea021fd6c5743f671611d9e0eff4c0e7737a4da9
N1femi <niffybabs@gmail.com>
2026-01-26 13:49:00 -0500

    Added a lil stuff ik its vague dont inquire

--- advisor.py
+++ advisor.py
@@ -1,3 +1,13 @@
+import pandas as pd
+import time
+
+# // VARIABLES \\ #
+default_params = {"part_number": "", "part_name": "", "manu_year": "", "part_brand": "Honda", "compat_car": ""}
+
+# // FUNCTIONS \\ #
+def retrieve_data():
+    return pd.read_csv("./data/parts.csv")
+
 def initiate_search() -> dict:
     part_number = input("Part Number? ")
     part_name = input("Part Name? ")
@@ -14,6 +24,16 @@ def initiate_search() -> dict:
         "compat_car": compat_car
     }
 
-
+def process_query(query_params= default_params):
+    data = retrieve_data()
+    print("Processing work: 99/100")
     
+    time.sleep(1)
     
+    print("Processing work: WE SHALL EAT TODAY")
+    
+    time.sleep(1)
+    
+    print(data.to_string())
+    
+process_query()
