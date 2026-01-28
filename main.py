ba6ae4a0275613e8df20bfbe66a71ccac8cf0192
N1femi <niffybabs@gmail.com>
2026-01-28 10:31:53 -0500

    Added focal inputs to main.py, I will treat that file simply as an I/O for the CLI

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ def main():
     gathering_info = True
     
     print("Welcome to MyPartAdvisor \n")
-    time.sleep(1)
     print(f"Skip unknown info or quit program ('quit') \n{'-' * 100} \n")
+    
+    part_number = input("\nPart Number? Enter: ")
+    part_name = input(f"{'-' * 5}\nPart Name? Enter: ")
+    part_brand = input(f"{'-' * 5}\nPart Brand? Enter: ")
+    compat_model = input(f"{'-' * 5}\nCompatible Model? Enter: ")
+    manu_year = input(f"{'-' * 5}\nManafactuar Year? Enter: ")
+        
+        
         
         
     
