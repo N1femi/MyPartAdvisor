0166b1906621d27d48d3e8bb1ce6f726e62bd678
N1femi <niffybabs@gmail.com>
2026-01-28 10:14:47 -0500

    Starting main.py Work

--- main.py
+++ main.py
@@ -1,26 +1,5 @@
-import pandas as pd
-
-
 def main():
-    data_path = "./data"
-    
-    target_name = input("What is the part you're looking for? ")
-    
-    dataset = pd.read_csv(f"{data_path}/parts.csv")
-    results = 0
-    
-    matches = dataset[dataset["part_name"].str.contains(target_name, case=False, na=False)].sort_values("year_start", ascending=False)
-    
-    
-    if matches.empty:
-        print("No results found..")
-    else:
-        print(matches[["part_name", "part_number", "year_start", "year_end"]].to_string(index=False))
-        print(f"Found {len(matches)} Results.")
-        
-        print(f"Top 2: {matches[["part_name", "part_number", "year_start", "year_end"]].head(2)}")
-        
-    
+    pass
     
     
 if __name__ == "__main__":
