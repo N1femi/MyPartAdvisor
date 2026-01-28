e6002902dbfe43a9c90c8ba1f3c8f388a5ef637a
N1femi <niffybabs@gmail.com>
2026-01-28 10:55:20 -0500

    Added loop for easier value replacement and stuff

--- main.py
+++ main.py
@@ -3,14 +3,30 @@ import time
 def main():
     gathering_info = True
     
+    query_params: dict[str, str | None] = {
+        "part_number": "Part Number?", 
+        "part_name": "Part Name?",
+        "part_brand": "Part Brand?",
+        "compat_model": "Compatible Model?",
+        "manu_year": "Manafactuar Year?"
+    }
+    
     print("Welcome to MyPartAdvisor \n")
     print(f"Skip unknown info or quit program ('quit') \n{'-' * 100} \n")
     
-    part_number = input("\nPart Number? Enter: ")
-    part_name = input(f"{'-' * 5}\nPart Name? Enter: ")
-    part_brand = input(f"{'-' * 5}\nPart Brand? Enter: ")
-    compat_model = input(f"{'-' * 5}\nCompatible Model? Enter: ")
-    manu_year = input(f"{'-' * 5}\nManafactuar Year? Enter: ")
+    for key in query_params:
+        currentInput = input(f"{'-'*5}\n{query_params[key]} Enter: ") # Consistent Input Request
+        currentInput = currentInput.strip()
+        
+        if currentInput == "":
+            currentInput = None
+            
+        query_params[key] = currentInput
+    
+    print("Done!", query_params)
+        
+    
+    #part_number = input("\nPart Number? Enter: ")
         
         
         
