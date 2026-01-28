ca544dd504de6a6f3ef5cf70f21ee9c881e80f63
N1femi <niffybabs@gmail.com>
2026-01-28 10:22:33 -0500

    Initializing CLI response

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import time
+
 def main():
-    pass
+    gathering_info = True
+    
+    print("Welcome to MyPartAdvisor \n")
+    time.sleep(1)
+    print(f"Skip unknown info or quit program ('quit') \n{'-' * 100} \n")
+        
+        
     
     
 if __name__ == "__main__":
